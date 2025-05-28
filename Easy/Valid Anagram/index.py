b6381f54c2b3fa7ebe3f887ab03c78b7fb4f1b10
PeytonCurr <peyton.curr@gmail.com>
2025-05-27 21:17:03 -0600

    Valid Anagram (No help)

--- Easy/Valid Anagram/index.py
+++ Easy/Valid Anagram/index.py
@@ -1,19 +1,32 @@
-# Sorting Solution
+# My Sorting Solution(No Help) - Time: O(nlogn + mlogm) / Space: O(1) or O(n + m)
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
-    
+  def isAnagram(self, s: str, t: str) -> bool:
+    if len(s) != len(t):
+       return False
+    sortedS = sorted(s)
+    sortedT = sorted(t)
+    for i in range(len(sortedS)):
+       if sortedS[i] != sortedT[i]:
+          return False
+    return True
+
+# Optimal Sorting Solution - Time: O(nlogn + mlogm) / Space: O(1) or O(n + m)
+class Solution:
+  def isAnagram(self, s: str, t: str) -> bool:
+    if len(s) != len(t):
+       return False
+    return sorted(s) == sorted(t)
 
 # Hashmap Solution
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False 
-        countS, countT = {}, {}
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
-        for k in countS:
-            if countS[k] != countT.get(k, 0):
-                return False
-        return True
+  def isAnagram(self, s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False 
+    countS, countT = {}, {}
+    for i in range(len(s)):
+        countS[s[i]] = 1 + countS.get(s[i], 0)
+        countT[t[i]] = 1 + countT.get(t[i], 0)
+    for k in countS:
+        if countS[k] != countT.get(k, 0):
+            return False
+    return True
