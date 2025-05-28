eff6a96623afb3e15d7f19d067f5f8bee8415041
PeytonCurr <peyton.curr@gmail.com>
2025-05-27 21:42:21 -0600

    Valid Anagram Hashmap Solution

--- Easy/Valid Anagram/index.py
+++ Easy/Valid Anagram/index.py
@@ -17,16 +17,13 @@ class Solution:
        return False
     return sorted(s) == sorted(t)
 
-# Hashmap Solution
+# Hashmap Solution - Time: O(n + m) / Space: O(1) Only Constant cause 26 Max Characters (Alphabet)
 class Solution:
   def isAnagram(self, s: str, t: str) -> bool:
-    if len(s) != len(t):
-        return False 
-    countS, countT = {}, {}
-    for i in range(len(s)):
-        countS[s[i]] = 1 + countS.get(s[i], 0)
-        countT[t[i]] = 1 + countT.get(t[i], 0)
-    for k in countS:
-        if countS[k] != countT.get(k, 0):
+        if len(s) != len(t):
             return False
-    return True
+        countS, countT = {}, {}
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
