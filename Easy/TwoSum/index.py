8ecf14de6a4f1557d429821a65192e381a45d7cb
PeytonCurr <peyton.curr@gmail.com>
2025-05-28 20:18:13 -0600

    Added Brute Force to TwoSum

--- Easy/TwoSum/index.py
+++ Easy/TwoSum/index.py
@@ -1,9 +1,18 @@
+#Brute Force - Nested Loop Solution
+class Solution:
+  def twoSum(self, nums: List[int], target: int) -> List[int]:
+    for i, _ in enumerate(nums):
+      for j, _ in enumerate(nums):
+        if nums[i] + nums[j] == target:
+          return [i,j]
+    return []
+
 #One Pass - Hashmap Solution
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevMap = {}
-        for i,n in enumerate(nums):
-            diff = target - n
-            if diff in prevMap:
-                return [prevMap[diff], i]
-            prevMap[n] = i
+  def twoSum(self, nums: List[int], target: int) -> List[int]:
+    prevMap = {}
+    for i,n in enumerate(nums):
+        diff = target - n
+        if diff in prevMap:
+            return [prevMap[diff], i]
+        prevMap[n] = i
