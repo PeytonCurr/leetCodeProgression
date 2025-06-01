b1d0c596feeef0701e6107176713c47d5b8c2a09
PeytonCurr <peyton.curr@gmail.com>
2025-06-01 15:04:37 -0600

    Group Anagrams Complete

--- Medium/GroupAnagrams/index.py
+++ Medium/GroupAnagrams/index.py
@@ -8,3 +8,15 @@ class Solution:
         res[sortedS] = []
       res[sortedS].append(s)
     return res.values()
+  
+
+#Hashmap Solution - Time: O(m * n) Space: O(m * n)
+class Solution:
+  def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    res = defaultdict(list)
+    for s in strs:
+      count = [0] * 26
+      for c in s:
+        count[ord(c) - ord('a')] += 1
+      res[tuple(count)].append(s)
+    return res.values()
