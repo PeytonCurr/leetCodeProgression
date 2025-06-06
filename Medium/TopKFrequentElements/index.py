fe38ba5882c5bf0807a213a7c7ea962a7bacddd3
PeytonCurr <peyton.curr@gmail.com>
2025-06-06 11:19:21 -0600

    Bucket Sort Solution

--- Medium/TopKFrequentElements/index.py
+++ Medium/TopKFrequentElements/index.py
@@ -13,7 +13,22 @@ class Solution:
             res.append(ary.pop()[1])
         return res
 
-#Hashmap Solution - Time: O() Space: O()
+#Bucket Sort Solution - Time: O(n) Space: O(n)
 class Solution:
   def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-      
+    class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        countA = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n,0)
+        for n, cnt in count.items():
+            countA[cnt].append(n)
+
+        res = []
+        for i in range(len(countA) - 1, 0 , -1):
+            for n in countA[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
