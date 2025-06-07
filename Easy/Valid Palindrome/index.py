43ee4caf55211f46896f75dd3f1fca4f6a15e324
PeytonCurr <peyton.curr@gmail.com>
2025-06-06 22:17:58 -0600

    Self Completing isPalindrome

--- Easy/Valid Palindrome/index.py
+++ Easy/Valid Palindrome/index.py
@@ -1,3 +1,4 @@
+#Optimal Two Pointer Solution - Time: O(n) Space: O(1)
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         l = 0
@@ -18,3 +19,16 @@ class Solution:
         return (ord('A') <= ord(c) <= ord('Z') or
                 ord('a') <= ord(c) <= ord('z') or
                 ord('0') <= ord(c) <= ord('9'))
+    
+    #My Two Pointer Solution - Time: O(n) Space: O(n)
+    def isPalindrome(self, s: str) -> bool:
+        res = []
+        for c in s:
+            if self.isAlphanumeric(c):
+                res.append(c.lower())
+        j = len(res)-1
+        for i,_ in enumerate(res):
+            if res[i] != res[j]:
+                return False
+            j -= 1
+        return True
