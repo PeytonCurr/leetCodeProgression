af7d9dbd32c0530176f2b08be5836e5ffac9f009
PeytonCurr <peyton.curr@gmail.com>
2025-06-10 21:46:07 -0600

    Binary Search

--- Easy/Binary Search/index.py
+++ Easy/Binary Search/index.py
@@ -1,14 +1,13 @@
+#Optimal Solution - Time: O(logn) Space: O(1)
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-       left = 0
-       right = len(nums) - 1
-
-       while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] > target:
-                right = mid - 1
-            elif nums[mid] < target:
-                left = mid + 1
+        l,r = 0, len(nums) - 1
+        while l <= r:
+            mid = l + ((r-l) //2)
+            if nums[mid] < target:
+                l = mid + 1
+            elif nums[mid] > target:
+                r = mid - 1
             else:
                 return mid
-       return -1 
+        return -1
