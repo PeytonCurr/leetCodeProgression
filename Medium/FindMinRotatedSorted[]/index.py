e2087c35386eccc11467624584e163364f342109
PeytonCurr <peyton.curr@gmail.com>
2025-06-11 14:55:31 -0600

    Find Min in Rotated Sorted Array

---
+++ Medium/FindMinRotatedSorted[]/index.py
@@ -0,0 +1,11 @@
+#Optimal Binary Search Solution - Time: O(logn) Space: O(1)
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l,r = 0, len(nums)-1
+        while l < r:
+            mid = l + ((r-l)//2)
+            if nums[mid] < nums[r]:
+                r = mid
+            else:
+                l = mid + 1
+        return nums[l]
