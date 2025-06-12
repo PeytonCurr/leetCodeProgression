0898b1cf332d9001968503146d586312a898cf5c
PeytonCurr <peyton.curr@gmail.com>
2025-06-12 14:11:03 -0600

    Merge two sorted lists

--- Easy/MergeTwoSortedLists/index.py
+++ Easy/MergeTwoSortedLists/index.py
@@ -1,21 +1,18 @@
+#Optimal Iterative Solution - Time: O(n+m) Space: O(1)
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode()
-        curNode = dummy
-
+        curr = dummy
         while list1 and list2:
             if list1.val < list2.val:
-                curNode.next = list1
+                curr.next = list1
                 list1 = list1.next
             else:
-                curNode.next = list2
+                curr.next = list2
                 list2 = list2.next
-            curNode = curNode.next
-
+            curr = curr.next
         if list1:
-            curNode.next = list1
-        
-        elif list2:
-            curNode.next = list2
-        
-        return dummy.next
+            curr.next = list1
+        if list2:
+            curr.next = list2
+        return dummy.next # Dummy.next points to Head while current is now at Tail
