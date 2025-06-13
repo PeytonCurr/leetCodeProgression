ba0aa1a026f4891e0ed13e95a0a95204291a7f64
PeytonCurr <peyton.curr@gmail.com>
2025-06-13 17:38:38 -0600

    Reverse and Merge Solution Completed

--- Medium/Reorder Linked List/index.py
+++ Medium/Reorder Linked List/index.py
@@ -1,4 +1,23 @@
 #Optimal Solution [Reverse and Merge] - Time: O(n) Space: O(1)
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
+        sp, fp = head, head.next
+        while fp and fp.next:
+            sp = sp.next
+            fp = fp.next.next
+
+        beginL2 = sp.next
+        prev = sp.next = None
+
+        while beginL2: 
+            tmp = beginL2.next
+            beginL2.next = prev
+            prev = beginL2
+            beginL2 = tmp
         
+        curr = head
+        while prev:
+            tmp, tmp2 = curr.next, prev.next
+            curr.next = prev
+            prev.next = tmp
+            curr, prev = tmp, tmp2
