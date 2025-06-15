b708846293e622acb0e48d72db842beb88d787b4
PeytonCurr <peyton.curr@gmail.com>
2025-06-15 14:38:07 -0600

    Invert Binary Tree - All Solutions

--- Easy/InvertBinaryTree/index.py
+++ Easy/InvertBinaryTree/index.py
@@ -1,6 +1,7 @@
+#Recursive DFS Solution - Time: O(n) Space: O(n)
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if root is None:
+        if not root:
             return None
         
         #swap the children
@@ -10,4 +11,38 @@ class Solution:
 
         self.invertTree(root.left)
         self.invertTree(root.right)
+        return root
+    
+#Iterative DFS Solution - Time: O(n) Space: O(n)
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            tmp = node.left
+            node.left = node.right
+            node.right = tmp
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return root
+
+#BFS Solution - Time: O(n) Space: O(n)
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            tmp = node.left
+            node.left = node.right
+            node.right = tmp
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
         return root
