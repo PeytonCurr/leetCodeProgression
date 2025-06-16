83bd1542864f3f0f414a7d8cc2925e5dce8d1ec8
PeytonCurr <peyton.curr@gmail.com>
2025-06-16 14:54:45 -0600

    Max Depth BFS

--- Easy/MaximumDepthBinaryTree/index.py
+++ Easy/MaximumDepthBinaryTree/index.py
@@ -1,6 +1,27 @@
+#DFS [Recursive] Solution - Time O(n) - Space O(n) 
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if root is None:
             return 0
 
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    
+
+
+
+#BFS Solution - Time O(n) - Space O(n) 
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        queue = deque([root])
+        level = 0
+        while queue:
+            for i in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            level += 1
+        return level
