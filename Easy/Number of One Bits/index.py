88c79d42c76617b0f1cf3c0c5abdb874964e82dd
PeytonCurr <peyton.curr@gmail.com>
2025-06-20 20:56:15 -0600

    Bit Mask Optimal

--- Easy/Number of One Bits/index.py
+++ Easy/Number of One Bits/index.py
@@ -6,3 +6,12 @@ class Solution:
             if n & (1 << i):
                 oneC += 1
         return oneC
+    
+#Bit Mask (Optimal) Solution: TO(1) - SO(1)
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        oneC = 0
+        while n != 0:
+            n = n&n-1
+            oneC += 1
+        return oneC
