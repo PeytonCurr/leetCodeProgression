efd936a7fcbbbae1718fd21ae616615e2ae1b1f5
PeytonCurr <peyton.curr@gmail.com>
2025-06-20 21:15:11 -0600

    Counting Bits

---
+++ Easy/Counting Bits/index.py
@@ -0,0 +1,11 @@
+#Bit Mask -2 Solution: TO(1) - SO(1)
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = []
+        for i in range(n+1):
+            count = 0
+            while i != 0:
+                i = i&i-1
+                count += 1
+            res.append(count)
+        return res
