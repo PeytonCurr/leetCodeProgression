39b2c13fc9c3a2f1e13a6f3296c6367c0624e08f
PeytonCurr <peyton.curr@gmail.com>
2025-06-26 11:27:40 -0600

    Iteration + Optimized

--- Easy/Climbing Stairs/index.py
+++ Easy/Climbing Stairs/index.py
@@ -17,6 +17,19 @@ class Solution:
         return res[n]
     
 #Tabulation Solution: TO(n) - SO(n)
-
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        res = {1:1, 2:2,3:3}
+        for i in range(4,n+1):
+            res[i] = res[i-1]+res[i-2]
+        return res[n]
 
 #[Optimized Space] Tabulation Solution: TO(n) - SO(n)
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        prev2, prev, curV = 0, 1, 1
+        for _ in range(1,n+1):
+            curV = prev + prev2
+            prev2 = prev
+            prev = curV
+        return curV
